2424dcd2eff20eb62c5a5b1ef66ed667f9e87757
Sapna Jha <sapnajha1919@gmail.com>
2023-03-11 21:42:38 +0530

    Documentation Update, added UI snapshots

--- PROJECT-README.md
+++ PROJECT-README.md
@@ -14,3 +14,33 @@ To use this project, follow these steps:
 * In the project directory, run npm install to install the required dependencies.
 * To start the app, run npm start in the project directory.
 * Open your browser and navigate to http://localhost:3000 to view the app.
+
+### Preview
+If evrything goes well, This login page will be shown in the browser:
+![UI snapshot for Home/Login page](./src/Assets/LoginPage.png)
+
+It can take any Email address and password for Authentication.
+It will look like this:
+![UI snapshot for Authentication](./src/Assets/Authentication.png)
+
+Click on Submit or Login with Google to redirect to Dashboard page.
+If everythind done well, you will see this page:
+![UI snapshot for Dashboard page](./src/Assets/Dashboard.png)
+
+#### How to use
+After reaching Dashboard page, you can import a csv file to json and save it to the localstorage by clicking on the "Import Student" button. Then you can export that converted Json string to you local storage of browser.
+
+* Import:
+--> It will open your file manager, select any csv file.
+--> If everything goes well, you will see an alert indicating successfull import.
+Look at the below snapshot for reference:
+![UI snapshot for Successfull import](./src/Assets/ImportSuccessfull.png)
+
+* Export:
+-->After successfull import, click on the export csv button.
+You will see a prompt indicating successfull export and your file will be downloaded:
+Take reference from this snapshot:
+![UI snapshot for Successfull import](./src/Assets/ExportSuccessfull.png)
+
+Done! Thank you for checking out.
+
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -34,18 +34,28 @@ export default function Dashboard() {
   function handleExport() {
     // Implement export functionality
     const jsonData = localStorage.getItem('StudentData');
-    const blob = new Blob([jsonData], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'student_data.json';
-    a.click();
-    // Display success message
-    alert('Export successful!');
+    if (jsonData) {
+      const blob = new Blob([jsonData], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'student_data.json';
+      a.click();
+      // Display success message
+      alert('Export successful!');
+    }
 
-
+    else {
+      alert('Fist import a csv file to export it successfully')
+    }
 
   }
+
+
+  const borderStyle = {
+    "border": "1px solid #EEEAF9",
+    "overflow": "hidden"
+  };
 
   return (
     <div className="flex h-screen justify-center items-center">
@@ -61,47 +71,49 @@ export default function Dashboard() {
           </div>
         </div>
         <p className="w-[290px] h-[24px] font-Inter text-base text-[#7D8398] font-normal pr-5 pb-10">List of all the students in the database</p>
-        <table className=" border-collapse border-[#EEEAF9] rounded-lg rounded-t-lg-[#EEEAF9] rounded-b-lg overflow-hidden">
-          <thead classNAme="rounded-tl-xl rounded-tr-xl mr-40">
-            <tr className="bg-[#F4F0FF] text-black font-Inter text-sm font-semibold  h-[41px] rounded-t-xl mr-40">
-              <th className="w-[144.6px] rounded-tl-md border-t-[#EEEAF9] pr-20">Name</th>
-              <th className="w-[144.6px] pr-20">Roll No</th>
-              <th className="w-[144.6px] pr-20">Address</th>
-              <th className="w-[144.6px] pr-20">Institute</th>
-              <th className="w-[144.6px] rounded-tr-md pr-20 border-l border-[#EEEAF9]">Course</th>
-            </tr>
-          </thead>
-          <tbody className="rounded-t-lg rounded-b-lg">
-            <tr className="border-b border-[#EEEAF9] h-[92px] text-sm">
-              <td className="pr-20 border-l border-[#EEEAF9]">Aarav</td>
-              <td className="pr-20">1962001</td>
-              <td className=""><div className="w-[144.6px] flex flex-col"><div className="text-left">10/1, Main Street,</div><div className="text-left">Gandhi Nagar,</div><div className="text-left">Bangalore - 560008,</div><div className="text-left">Karnataka</div></div></td>
-              <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">LMN Institute of</div><div className="text-left">Technology</div></div></td>
-              <td className="border-r border-[#EEEAF9]"><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">Bachelors of</div><div className="text-left">Technology</div></div></td>
-            </tr>
-            <tr className="border-b border-[#EEEAF9]  h-[109px] text-sm">
-              <td className="pr-20 border-l border-[#EEEAF9]">Dhruv</td>
-              <td className="pr-20">1473502</td>
-              <td className=""><div className="w-[144.6px] flex flex-col"><div className="text-left">Door No. 12-2-417/A,</div><div className="text-left">Bank Colony Road,</div><div className="text-left">Ruknuddin Bagh,</div><div className="text-left">Hyderabad - 500032,</div><div className="text-left">Telangana</div></div></td>
-              <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">DEF Management</div><div className="text-left">Institute</div></div></td>
-              <td className="border-r border-[#EEEAF9] "><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">Masters of Business</div><div className="text-left">Administration</div></div></td>
-            </tr>
-            <tr className="border-b border-[#EEEAF9] h-[109px] text-sm">
-              <td className="pr-20 border-l border-[#EEEAF9]">Sahil</td>
-              <td className="pr-20">1762503</td>
-              <td className=""><div className="w-[144.6px] flex flex-col"><div className="text-left">H.No. 8-2-123/456,</div><div className="text-left">Sri Nagar Colony,</div><div className="text-left">Road No. 1, Banjara</div><div className="text-left">Hills, Hyderabad -</div><div className="text-left">500034, Telangana</div></div></td>
-              <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">MNO Medical College</div></div></td>
-              <td className="border-r border-[#EEEAF9]"><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">Bachelor of Unani</div><div className="text-left">Medicine and</div><div className="text-left">Surgery</div></div></td>
-            </tr>
-            <tr className="h-[51px] text-sm rounded-md">
-              <td className="rounded-bl-md border-b border-[#EEEAF9] border-l border-[#EEEAF9] rounded-bl-lg"></td>
-              <td className="rounded-br-md border-b border-[#EEEAF9]"></td>
-              <td className="rounded-br-md border-b border-[#EEEAF9]"></td>
-              <td className="rounded-br-md border-b border-[#EEEAF9]"></td>
-              <td className="border-b border-[#EEEAF9] border-r border-[#EEEAF9]"></td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="border rounded-lg overflow-hidden m-0">
+          <table>
+            <thead>
+              <tr className="bg-[#F4F0FF] text-black font-Inter text-sm font-semibold h-[41px]" >
+                <th className="w-[144.6px] pr-20">Name</th>
+                <th className="w-[144.6px] pr-20">Roll No</th>
+                <th className="w-[144.6px] pr-20">Address</th>
+                <th className="w-[144.6px] pr-20" >Institute</th>
+                <th className="w-[144.6px] pr-20">Course</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className=" h-[92px] text-sm" >
+                <td className="pr-20">Aarav</td>
+                <td className="pr-20">1962001</td>
+                <td className=""><div className="w-[144.6px] flex flex-col"><div className="text-left">10/1, Main Street,</div><div className="text-left">Gandhi Nagar,</div><div className="text-left">Bangalore - 560008,</div><div className="text-left">Karnataka</div></div></td>
+                <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">LMN Institute of</div><div className="text-left">Technology</div></div></td>
+                <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">Bachelors of</div><div className="text-left">Technology</div></div></td>
+              </tr>
+              <tr className="h-[109px] text-sm border-t border-[#EEEAF9]">
+                <td className="pr-20" >Dhruv</td>
+                <td className="pr-20" >1473502</td>
+                <td className=""><div className="w-[144.6px] flex flex-col"><div className="text-left">Door No. 12-2-417/A,</div><div className="text-left">Bank Colony Road,</div><div className="text-left">Ruknuddin Bagh,</div><div className="text-left">Hyderabad - 500032,</div><div className="text-left">Telangana</div></div></td>
+                <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">DEF Management</div><div className="text-left">Institute</div></div></td>
+                <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">Masters of Business</div><div className="text-left">Administration</div></div></td>
+              </tr>
+              <tr className="h-[109px] text-sm border-t border-[#EEEAF9]">
+                <td className="pr-20" >Sahil</td>
+                <td className="pr-20" >1762503</td>
+                <td className=""><div className="w-[144.6px] flex flex-col"><div className="text-left">H.No. 8-2-123/456,</div><div className="text-left">Sri Nagar Colony,</div><div className="text-left">Road No. 1, Banjara</div><div className="text-left">Hills, Hyderabad -</div><div className="text-left">500034, Telangana</div></div></td>
+                <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">MNO Medical College</div></div></td>
+                <td className=""><div className="w-[144.6px] flex flex-col ml-5"><div className="text-left">Bachelor of Unani</div><div className="text-left">Medicine and</div><div className="text-left">Surgery</div></div></td>
+              </tr>
+              <tr className="h-[51px] text-sm border-t border-[#EEEAF9]" >
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
